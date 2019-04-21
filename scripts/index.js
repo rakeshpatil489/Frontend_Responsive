57629e73546c2cf9de2c64ed969bc4bf7f524588
rakeshpatil <rakeshpatil489@gmail.com>
2019-04-21 12:53:02 +0200

    onclick event for Hamburger Icon

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+var hamburgerOpenBtn = document.querySelector('.hamburgerBtnShow'); /* catching hamburger Open icon */
+
+var hamburgerCloseBtn = document.querySelector('.hamburgerBtnClose');/* catching hamburger Close icon */
+
+var menu =document.querySelector('.menu'); /* catching menu content */
+
+
+/* click event on opening hamburger item*/
+hamburgerOpenBtn.addEventListener('click',function(){
+    
+   menu.style.right=0;
+
+})
+/* click event on closing hamburger item*/
+hamburgerCloseBtn.addEventListener('click',function(){
+    
+    menu.style.right='-100%';
+    
+})
